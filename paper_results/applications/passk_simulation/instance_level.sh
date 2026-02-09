187a6c0c79d638a9e5773804d66951617d3dbaed
Sin-Han Yang <sinhan.yang@appier.com>
2026-02-09 17:22:36 +0800

    feat(shy): update passk simulation code

---
+++ paper_results/applications/passk_simulation/instance_level.sh
@@ -0,0 +1,6 @@
+# Run PASS-K simulation at the instance level
+
+cd applications/passk_simulation
+
+uv run python src/evaluate_instance_passk.py \
+    --config config/instance_passk_evaluation.yaml
